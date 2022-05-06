86036ca5154d0b2390a357a121be717a52dbe0c5
Navodit20 <103431324+Navodit20@users.noreply.github.com>
2022-05-06 23:33:56 +0530

    Update assignment 2.py

--- assignment 2.py
+++ assignment 2.py
@@ -3,7 +3,7 @@
 # SID  : 21107082
 #Branch: Mechanical
   
-#Q1
+print('Q1')
 str1 = "Python is a case sensitive language"
 #a
 print("Length of the input string:", len(str1))
@@ -24,7 +24,7 @@ str4 = str1.replace(' ','')
 print(str4)
 
 
-#Q2
+print('Q2')
 name = "Navodit Gupta"
 SID = 21107082
 Dept = "Mechanical"
@@ -33,7 +33,7 @@ CGPA = 8.7
 print(f"Hey, {name} Here! \nMy SID is {SID} \nI am from {Dept} and my CGPA is {CGPA}")
 
 
-#Q3
+print('Q3')
 a,b = 56,10
 print(a&b)
 print(a|b)
@@ -42,7 +42,7 @@ print(a<<2,b<<2)
 print(a>>2,b>>4)
 
 
-#Q4
+print('Q4')
 str1 = input("Enter a string:")
 if 'name' in str1:
     print('Yes')
@@ -50,7 +50,7 @@ else:
     print("No")
 
 
-#Q5
+print('Q5')
 a = int(input("Enter triangle side 1:"))
 b = int(input("Enter triangle side 2:"))
 c = int(input("Enter triangle side 3:"))
@@ -62,13 +62,13 @@ match e:
     case False:
         print('No')
 
-#Q6
+print('Q6')
 
 a = int(input("Enter first number:"))
 b = int(input("Enter second number:"))
 c = bin(a^b)     #convert new number to binary
 d = c.count('1') #count the bits needed to be changed
-print(d)
+print('Number of bits to be flipped:',d)
 
 
 
