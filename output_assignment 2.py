0863852c24a7dfccfe07c0d6e1546f2f6513fd81
Navodit20 <103431324+Navodit20@users.noreply.github.com>
2022-05-06 23:31:49 +0530

    Add files via upload

---
+++ output_assignment 2.py
@@ -0,0 +1,32 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Navodit\AppData\Local\Programs\Python\Python310\assignments\assignment 2.py
+Q1
+Length of the input string: 35
+egaugnal evitisnes esac a si nohtyP
+object oriented
+10
+Pythonisacasesensitivelanguage
+Q2
+Hey, Navodit Gupta Here! 
+My SID is 21107082 
+I am from Mechanical and my CGPA is 8.7
+Q3
+8
+58
+50
+224 40
+14 0
+Q4
+Enter a string:My name is Navodit
+Yes
+Q5
+Enter triangle side 1:8
+Enter triangle side 2:3
+Enter triangle side 3:6
+Yes
+Q6
+Enter first number:45
+Enter second number:32
+Number of bits to be flipped: 3
