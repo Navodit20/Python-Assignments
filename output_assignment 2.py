b5b2ea4705d171d913c284b2e2fe47c8df88cf58
Navodit20 <103431324+Navodit20@users.noreply.github.com>
2022-05-06 23:27:14 +0530

    Add files via upload

---
+++ output_assignment 2.py
@@ -0,0 +1,26 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Navodit\AppData\Local\Programs\Python\Python310\assignments\assignment 2.py
+Length of the input string: 35
+egaugnal evitisnes esac a si nohtyP
+object oriented
+10
+Pythonisacasesensitivelanguage
+Hey, Navodit Gupta Here! 
+My SID is 21107082 
+I am from Mechanical and my CGPA is 8.7
+8
+58
+50
+224 40
+14 0
+Enter a string:My name is Navodit
+Yes
+Enter triangle side 1:4
+Enter triangle side 2:8
+Enter triangle side 3:3
+No
+Enter first number:24
+Enter second number:14
+3
