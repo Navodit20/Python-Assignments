cf4e74f571017ce4a3dfba5271155dcc418a786c
Navodit20 <103431324+Navodit20@users.noreply.github.com>
2022-05-12 17:56:14 +0530

    Add files via upload

---
+++ assignment 3 output.py
@@ -0,0 +1,51 @@
+Q1
+Enter a number:23
+Binary equivalent of 23 is 10111
+Q2
+Enter expression:32-63+(56/7+2)**2
+32-63+(56/7+2)**2 = 69.0
+Q3
+Enter an integer:3
+a) 35
+b) 3.0
+enter radius4
+c) 201.06192982974676
+Enter angle 1:60
+Enter angle 1:30
+d) 2.7446653533506478
+Enter x coordinate of first point:3
+Enter y coordinate of first point:6
+Enter x coordinate of second point:1
+Enter y coordinate of second point:4
+e) 1.0
+Q4
+4.(a)
+0
+1
+2
+3
+4
+4.(b)
+3
+4
+5
+6
+7
+8
+9
+4.(c)
+4
+7
+10
+4.(d)
+15
+13
+11
+9
+7
+4.(e)
+Q5
+Enter number of carbon atoms in molecule:0
+Enter number of hydrogen atoms in molecule:2
+Enter number of oxygen atoms in molecule:1
+The molecular weight of the compound is 18.01528
