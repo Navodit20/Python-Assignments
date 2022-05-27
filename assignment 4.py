63d26bcbfe306cc07e8891911a5d4518145c109f
Navodit20 <103431324+Navodit20@users.noreply.github.com>
2022-05-27 21:30:02 +0530

    Update assignment 4.py

--- assignment 4.py
+++ assignment 4.py
@@ -25,7 +25,10 @@ print('Q2')
 year = int(input('Enter year:'))
 if year % 4 == 0:
     if year % 100 == 0:
-        print("Leap year")
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not a leap year")
     else:
         print("Not a leap year")
 else:
