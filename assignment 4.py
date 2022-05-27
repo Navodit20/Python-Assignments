1bfd4ec1faf95847e8607abdc7ad519a5835a0f9
Navodit20 <103431324+Navodit20@users.noreply.github.com>
2022-05-27 22:14:29 +0530

    Update assignment 4.py

--- assignment 4.py
+++ assignment 4.py
@@ -3,23 +3,20 @@
 #SID: 21107082
 #Branch: Mechanical
 print("Q1")
-while True:
-    mark = int(input("Enter mark:"))
-    if mark < 25:
-        print("Grade: F")
-    elif 25 <= mark < 45:
-        print("Grade: E")
-    elif 45 <= mark < 50:
-        print("Grade: D")
-    elif 50 <= mark < 60:
-        print("Grade: C")
-    elif 60 <= mark < 80:
-        print("Grade: B")
-    elif mark >= 80:
-        print("Grade: A")
-    user = input("Continue?(y/n)")
-    if user == 'n':
-        break
+mark = int(input("Enter mark:"))
+if mark < 25:
+    print("Grade: F")
+elif 25 <= mark < 45:
+    print("Grade: E")
+elif 45 <= mark < 50:
+    print("Grade: D")
+elif 50 <= mark < 60:
+    print("Grade: C")
+elif 60 <= mark < 80:
+    print("Grade: B")
+elif mark >= 80:
+    print("Grade: A")
+
         
 print('Q2')
 year = int(input('Enter year:'))
