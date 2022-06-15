27a065a1cfc611e667e022758e0fa7d5b1c5a69f
Navodit20 <103431324+Navodit20@users.noreply.github.com>
2022-06-15 22:54:00 +0530

    Update assignment 5 output.py

--- assignment 5 output.py
+++ assignment 5 output.py
@@ -1,8 +1,3 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-eval(input([1,2,3,'se']))
-[1, 2, 3, 'se']
-= RESTART: C:/Users/Navodit/AppData/Local/Programs/Python/Python310/assignments/assignment 5.py
 Q1
 Enter a string(without apostrophes) and it will be reversed:Hello world
 Reversed string: dlrow olleH
