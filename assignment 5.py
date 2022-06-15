c30f27a19e766a6b1e04c89df51bd497cc56d87f
Navodit20 <103431324+Navodit20@users.noreply.github.com>
2022-06-15 23:30:34 +0530

    Update assignment 5.py

--- assignment 5.py
+++ assignment 5.py
@@ -42,7 +42,7 @@ n = int(input("Enter number of rows:"))
 k = 65
 for i in range(n):
     for j in range(i+1):
-        print(chr(k), end=" ")
+        print(chr(k), end="")
         k = k + 1
     print()
 
